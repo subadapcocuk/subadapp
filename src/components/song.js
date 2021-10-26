e26ff5ab988f311d7616ef0b260efdc32eabd598
T. E. Kalaycı <tekrei@gmail.com>
2021-10-26 06:33:41 +0200

    [#19] Remove album and year from song title in playlist (#21)

--- src/components/song.js
+++ src/components/song.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image, ScrollView, TouchableOpacity, Text, View } from "react-native";
-import { getSongTitle } from "../api/data";
+import { getAlbumTitle } from "../api/data";
 import { styles, songStyle, songText, deviceWidth } from "../helpers/styles";
 
 export const Song = ({ song, openUrl }) => {
@@ -21,7 +21,7 @@ export const SongItem = ({ song, selected = false }) => {
   return (
     <View style={songStyle(selected)}>
       <Image style={styles.playlistImage} source={{ uri: song.image }} />
-      <Text style={songText(selected)}>{getSongTitle(song)}</Text>
+      <Text style={songText(selected)}>{song.name}</Text>
     </View>
   );
 };
@@ -35,9 +35,8 @@ export const SongDetail = ({ song, openUrl }) => {
           onPress={() => openUrl(song.page)}
         >
           <Image style={styles.albumImage} source={{ uri: song.image }} />
-          <Text>
-            {song.no} - {song.name}
-          </Text>
+          <Text style={{ fontSize: 32 }}>{song.name}</Text>
+          <Text style={{ fontSize: 16 }}>{getAlbumTitle(song.albumNo)}</Text>
         </TouchableOpacity>
       ) : (
         <Text style={styles.albumTitle}>Şu an şarkı çalmıyor</Text>
